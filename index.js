15f8d5344ce74689f5569fb27620e766f98701f3
Amanuel <49632606+amantes30@users.noreply.github.com>
2024-07-10 15:28:02 +0800

    modify

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const fs = require('fs').promises;
-const path = require('path');
-const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
-
+const fs = require("fs").promises;
+const path = require("path");
+const process = require("process");
+const { authenticate } = require("@google-cloud/local-auth");
+const { google } = require("googleapis");
+const express = require("express");
+const app = express();
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
-
+const TOKEN_PATH = path.join(process.cwd(), "token.json");
+const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
 
 // Reads previously authorized credentials from the save file.
 
@@ -25,14 +25,13 @@ async function loadSavedCredentialsIfExist() {
   }
 }
 
-
 // Serializes credentials to a file compatible with GoogleAuth.fromJSON.
 async function saveCredentials(client) {
   const content = await fs.readFile(CREDENTIALS_PATH);
   const keys = JSON.parse(content);
   const key = keys.installed || keys.web;
   const payload = JSON.stringify({
-    type: 'authorized_user',
+    type: "authorized_user",
     client_id: key.client_id,
     client_secret: key.client_secret,
     refresh_token: client.credentials.refresh_token,
@@ -65,13 +64,27 @@ async function authorize() {
 
 // check number of unread messages
 async function getUnreadMessages(auth) {
-  const gmail = google.gmail({version: 'v1', auth});
+  const gmail = google.gmail({ version: "v1", auth });
   const res = await gmail.users.messages.list({
-    userId: 'me',
-    q: 'is:unread',
+    userId: "me",
+    q: "is:unread",
   });
-  console.log('Number of unread messages:', res.data.resultSizeEstimate);
+  return `Number of unread messages:, ${res.data.resultSizeEstimate}`;
 }
 
+// authorize()
+//   .then((auth) => {
+//     getUnreadMessages(auth);
+//   })
+//   .catch(console.error);
 
-authorize().then(getUnreadMessages).catch(console.error);
+app.get("/", (req, res) => {
+  authorize()
+    .then((auth) => {
+      res.send(getUnreadMessages(auth));
+    })
+    .catch(console.error);
+});
+app.listen(3000, () => {
+  console.log("Server is running on 3000");
+});
